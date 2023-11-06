574623c4b5b994e2e060861b92d6e440badd540e
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2023-11-06 09:18:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
-
 import { Route, Routes } from "react-router-dom";
 
 function App() {
