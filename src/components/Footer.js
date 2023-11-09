9efcf46ba59c7589ff8ddb8e7633a7bc6a1774b5
Arka Ghosh Chowdhury <arkaghoshchowdhury@gmail.com>
2023-11-09 09:34:11 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import "./FooterStyles.css";
-
 import React from 'react';
 import {FaHome, FaPhone, FaMailBulk, FaFacebook, FaTwitter, FaLinkedin} from "react-icons/fa";
 
@@ -44,5 +43,4 @@ const Footer = () => {
     </div>
   )
 }
-
 export default Footer
